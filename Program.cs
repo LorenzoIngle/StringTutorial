abe80ddb82e8e633d10f94c1ee659bc289888b45
LorenzoIngle <119593905+LorenzoIngle@users.noreply.github.com>
2025-02-17 18:04:08 +0000

    update

--- Program.cs
+++ Program.cs
@@ -11,18 +11,23 @@ namespace StringTutorial
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a string to encrypt:");
-            
-
+            string userInput = Console.ReadLine();
+            string encryptedString = " ";
             try
             {
-                
+                 encryptedString = EncryptString(userInput);
             }
-            catch ()
+            catch (ArgumentNullException)
             {
-                  
+                Console.WriteLine("no input detected");
+            }
+            catch (ArgumentOutOfRangeException) 
+            {
+                Console.WriteLine("input is wrong format");
             }
             finally
             {
+                Console.WriteLine(encryptedString);
                 Console.WriteLine("Press any key to exit...");
                 Console.ReadKey();
             }
@@ -31,20 +36,32 @@ namespace StringTutorial
         static string EncryptString(string inputString)
         {
             // Guard clause to check if input is a valid string
-            
+            if (string.IsNullOrEmpty(inputString))
+            {
+                throw new ArgumentNullException(nameof(inputString), "Input string must not be null or empty");
+            }
             // Reverse the string
-            
+            char[] temp = inputString.ToCharArray();
+            Array.Reverse(temp);
+            string reverseString = new String(temp);
 
             // Convert every second charatcer to uppercase
-           
+            for (int i = 0; i<reverseString.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Char.ToUpper(reverseString[i]);
+                }
+            }
 
             // Interpolateion and concatenation
-          
+
 
             // String conversion using ASCII values to shift each character by 1
-            
-            
-            string finalEncryption = new string(finalEncryptionChars);
+
+
+            //string finalEncryption = new string(finalEncryptionChars);
+            string finalEncryption = reverseString;
             return finalEncryption;
         }
     }
